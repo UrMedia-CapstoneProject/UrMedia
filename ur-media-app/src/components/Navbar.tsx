6a7687395308832e16098cb813bfeca904f8763c
AustinRowland29 <fastang1994@gmail.com>
2026-03-08 00:12:13 -0600

    updated navbar

--- ur-media-app/src/components/Navbar.tsx
+++ ur-media-app/src/components/Navbar.tsx
@@ -3,22 +3,36 @@ import Image from "next/image"
 
 export default function Navbar() {
     return (
-        <nav style={{display: "flex", justifyContent: "space-between", backgroundColor: "#26007a"}}>
-            <div style={{display: "flex"}}>
+        <nav style={{display: "flex", justifyContent: "space-between", backgroundColor: "#26007a", padding: "10px"}}>
+            <div style={{display: "flex", height: "40px"}}>
                 <Link href="/catalog">
                 <Image
-                src="/test-images/madoka.png"
+                src="/navbar-icons/browse.png"
+                title="Browse Catalog"
                 alt="Browse"
                 width={40}
                 height={40}
+                style={{marginRight: "15px"}}
                 />
                 </Link>
-                <input type="text" placeholder="Search..."/>
+                <div style={{display: "flex", backgroundColor: "#200160", borderRadius:"20px"}}>
+                    <input type="text" placeholder="Search..." style={{marginLeft:"4px"}}/>
+                    <Link href="/catalog" style={{margin: "5px"}}>
+                        <Image
+                        src="/navbar-icons/search.png"
+                        title="Search"
+                        alt="Search"
+                        width={25}
+                        height={20}
+                        style={{marginRight:"2px"}}
+                        />
+                    </Link>
+                </div>
             </div>
             <Link href="/">
-                <h1>UrMedia</h1>
+                <h1 style={{fontSize: "2em", color: "#4e5150"}}>UrMedia</h1>
             </Link>
-            <div style={{margin: "10px", padding: "10px"}}>
+            <div>
                 <Link href="profile">
                     <Image
                     src="/test-images/azusa.jpg"
