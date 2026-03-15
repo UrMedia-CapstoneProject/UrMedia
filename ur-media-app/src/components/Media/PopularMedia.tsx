806999f8987de91803eca89cdea9611648ba63d1
AustinRowland29 <fastang1994@gmail.com>
2026-03-15 17:35:34 -0500

    Updated css for media page

--- ur-media-app/src/components/Media/PopularMedia.tsx
+++ ur-media-app/src/components/Media/PopularMedia.tsx
@@ -6,19 +6,19 @@ export default function PopularMedia() {
     return(
         <div className={styles.main}>
             <div>
-                <h2>Popular Movies</h2>
+                <h2 className={styles.sectionHeader}>Popular Movies</h2>
                 <MediaGrid />
             </div>
             <div>
-                <h2>Popular Shows</h2>
+                <h2 className={styles.sectionHeader}>Popular Shows</h2>
                 <MediaGrid />
             </div>
             <div>
-                <h2>Popular Games</h2>
+                <h2 className={styles.sectionHeader}>Popular Games</h2>
                 <MediaGrid />
             </div>
             <div>
-                <h2>Popular Books</h2>
+                <h2 className={styles.sectionHeader}>Popular Books</h2>
                 <MediaGrid />
             </div>
         </div>
