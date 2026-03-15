e8ae51de7fde7e03af480fff2ca5c63dbb754f13
Bethany Rowland <yorkiegirlzoe2001@gmail.com>
2026-03-15 17:22:17 -0500

    Profile Page work
    trying to fix the branch

--- ur-media-app/src/app/page.tsx
+++ ur-media-app/src/app/page.tsx
@@ -1,17 +1,8 @@
-<<<<<<< Updated upstream
 import Image from "next/image";
-=======
 import styles from "./page.module.css"
 import PopularMedia from "@/components/Media/PopularMedia";
 import Countdown from "@/components/Media/Countdown";
 import Friends from "@/components/Media/Friends";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 export default function Home() {
   return (
