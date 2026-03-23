aac621808148efcafe3b9a878a682c3b8d4314af
AustinRowland29 <fastang1994@gmail.com>
2026-03-23 14:27:23 -0500

    FJKDFJDKFDJKFDJ

--- ur-media-app/src/components/Media/Countdown.tsx
+++ ur-media-app/src/components/Media/Countdown.tsx
@@ -9,31 +9,35 @@ export default function() {
     return (
         <div className={styles.main}>
             <h2 className={styles.sectionHeader}>Countdown</h2>
+
+            <div className={styles.content}>
             
-            <div className={styles.tabBar}>
-                <button className={`${styles.tabLink} ${activeTab === "movies" ? styles.active : ""}`}
-                    onClick={() => setActiveTab("movies")}
-                >Movies</button>
+                <div className={styles.tabBar}>
+                    <button className={`${styles.tabLink} ${activeTab === "movies" ? styles.active : ""}`}
+                        onClick={() => setActiveTab("movies")}
+                    >Movies</button>
 
-                <button className={`${styles.tabLink} ${activeTab === "shows" ? styles.active : ""}`}
-                    onClick={() => setActiveTab("shows")}
-                >Shows</button>
+                    <button className={`${styles.tabLink} ${activeTab === "shows" ? styles.active : ""}`}
+                        onClick={() => setActiveTab("shows")}
+                    >Shows</button>
 
-                <button className={`${styles.tabLink} ${activeTab === "games" ? styles.active : ""}`}
-                    onClick={() => setActiveTab("games")}
-                >Games</button>
+                    <button className={`${styles.tabLink} ${activeTab === "games" ? styles.active : ""}`}
+                        onClick={() => setActiveTab("games")}
+                    >Games</button>
 
-                <button className={`${styles.tabLink} ${activeTab === "books" ? styles.active : ""}`}
-                    onClick={() => setActiveTab("books")}
-                >Books</button>
+                    <button className={`${styles.tabLink} ${activeTab === "books" ? styles.active : ""}`}
+                        onClick={() => setActiveTab("books")}
+                    >Books</button>
 
-            </div>
+                </div>
 
-            <div className={styles.tabContent}>
-                {activeTab === "movies" && <p>Upcoming Movies</p>}
-                {activeTab === "shows" && <p>Upcoming Shows</p>}
-                {activeTab === "games" && <p>Upcoming Games</p>}
-                {activeTab === "books" && <p>Upcoming Books</p>}
+                <div className={styles.tabContent}>
+                    {activeTab === "movies" && <p>Upcoming Movies</p>}
+                    {activeTab === "shows" && <p>Upcoming Shows</p>}
+                    {activeTab === "games" && <p>Upcoming Games</p>}
+                    {activeTab === "books" && <p>Upcoming Books</p>}
+                </div>
+
             </div>
         </div>
     )
