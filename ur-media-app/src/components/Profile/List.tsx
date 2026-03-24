b0d5d76d71d34486a6f0c9c4cdd51dcdf9809fed
Bethany Rowland <yorkiegirlzoe2001@gmail.com>
2026-03-24 11:44:00 -0500

    Update List.tsx

--- ur-media-app/src/components/Profile/List.tsx
+++ ur-media-app/src/components/Profile/List.tsx
@@ -2,16 +2,10 @@ import styles from "./List.module.css"
 
 export default function () {
     return (
-        /*
         <div className={styles.main}>
             <div className={styles.tabList}>
-                <button className="tab active" onclick="showTab('tab1', this)">Tab 1</button>
-                <button className="tab" onclick="showTab('tab2', this)">Tab 2</button>
-                <button className="tab" onclick="showTab('tab3', this)">Tab 3</button>
-                <button className="tab" onclick="showTab('tab4', this)">Tab 4</button>
+                
             </div>
         </div>
-        */
-       1
     )
 }
