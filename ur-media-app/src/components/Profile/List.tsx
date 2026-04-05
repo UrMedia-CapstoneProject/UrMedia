8f6c7abd93794d86d37baa765981f55d266f7e00
Bethany Rowland <yorkiegirlzoe2001@gmail.com>
2026-04-04 23:44:29 -0500

    Filter button prototype
    Finished the design for filter button and added placeholder code for function.

--- ur-media-app/src/components/Profile/List.tsx
+++ ur-media-app/src/components/Profile/List.tsx
@@ -6,6 +6,12 @@ import MediaList from "@/components/Profile/MediaList";
 export default function () {
 
     const [activeTab, setActiveTab] = useState("movies")
+    const [activeFilter, setActiveFilter] = useState<string | null>("A-Z")
+
+    function handleFilterSelect(filter: string) {
+        setActiveFilter(filter)
+        setActiveTab("filter")
+    }
 
     return (
         <div className={styles.main}>
@@ -26,9 +32,18 @@ export default function () {
                     onClick={() => setActiveTab("books")}
                 >Books</button>
 
-                <button className={`${styles.tabLink} ${activeTab === "filter" ? styles.active : ""}`}
-                    onClick={() => setActiveTab("filter")}
-                >Filter</button>
+                <div className={styles.dropdown}>
+                    <button className={`${styles.filterButton} ${activeTab === "filter"}`}>
+                        {activeFilter}
+                    </button>
+
+                    <div className={styles.dropdownMenu}>
+                        <button onClick={() => handleFilterSelect("A-Z")}>A–Z</button>
+                        <button onClick={() => handleFilterSelect("Z-A")}>Z–A</button>
+                        <button onClick={() => handleFilterSelect("Rating")}>Rating</button>
+                        <button onClick={() => handleFilterSelect("Date Added")}>Date Added</button>
+                    </div>
+                </div>
             </div>
 
             <div className={styles.tabContent}>
@@ -36,7 +51,7 @@ export default function () {
                 {activeTab === "shows" && <MediaList />}
                 {activeTab === "games" && <MediaList />}
                 {activeTab === "books" && <MediaList />}
-                {activeTab === "filter" && <p>Filter</p>}
+                {activeTab === "filter" && <p>Filtering by: {activeFilter}</p>}
             </div>
         </div>
     )
