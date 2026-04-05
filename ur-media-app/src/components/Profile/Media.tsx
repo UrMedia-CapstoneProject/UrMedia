eca29f95cc3ad9a4f9ec172fb21ccb8fca36b213
Bethany Rowland <yorkiegirlzoe2001@gmail.com>
2026-04-05 15:55:29 -0500

    Poster fix
    Fixed the posters to display database info and made media cards for the list.

---
+++ ur-media-app/src/components/Profile/Media.tsx
@@ -0,0 +1,23 @@
+"use client"
+import styles from "./Media.module.css"
+import { useState } from "react"
+import Poster from "@/components/Global/Poster";
+
+export default function () {
+    return (
+        <div className={styles.main}>
+            <div className={styles.poster}>
+                <Poster title = {"The Odyssey"} imageUrl = {"/test-images/the-odyssey.jpg"}/>
+            </div>
+
+            <div className={styles.info}>
+                <p>{"The Odyssey"}</p>
+
+                <div className={styles.userInfo}>
+                    <p>Status: Plan to Watch</p>
+                    <p>10/10</p>
+                </div>
+            </div>
+        </div>
+    )
+}
