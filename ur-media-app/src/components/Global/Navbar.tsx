ff9acb3f5e3fbf95e35afc2363de57b6d0af02b8
AustinRowland29 <fastang1994@gmail.com>
2026-04-06 11:28:06 -0500

    made navbar sticky, replaced colors with css variables)

--- ur-media-app/src/components/Global/Navbar.tsx
+++ ur-media-app/src/components/Global/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useEffect, useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
 import styles from "./Navbar.module.css"
@@ -8,23 +10,23 @@ export default function Navbar() {
             <div className={styles.search}>
                 <Link href="/catalog">
                     <Image
-                    src="/navbar-icons/browse.png"
-                    title="Browse Catalog"
-                    alt="Browse"
-                    width={40}
-                    height={40}
-                    className={styles.browseIcon}
+                        src="/navbar-icons/browse.png"
+                        title="Browse Catalog"
+                        alt="Browse"
+                        width={40}
+                        height={40}
+                        className={styles.browseIcon}
                     />
                 </Link>
                 <div className={styles.searchBar}>
-                    <input type="text" placeholder="Search..." className={styles.searchBarInput}/>
+                    <input type="text" placeholder="Search..." className={styles.searchBarInput} />
                     <Link href="/catalog" className={styles.searchBarButton}>
                         <Image
-                        src="/navbar-icons/search.png"
-                        title="Search"
-                        alt="Search"
-                        width={25}
-                        height={20}
+                            src="/navbar-icons/search.png"
+                            title="Search"
+                            alt="Search"
+                            width={25}
+                            height={20}
                         />
                     </Link>
                 </div>
@@ -35,15 +37,15 @@ export default function Navbar() {
             <div className={styles.profile}>
                 <Link href="profile">
                     <Image
-                    src="/test-images/azusa.jpg"
-                    title="Username"
-                    alt="Profile"
-                    width={45}
-                    height={45}
-                    className={styles.pfp}
+                        src="/test-images/azusa.jpg"
+                        title="Username"
+                        alt="Profile"
+                        width={45}
+                        height={45}
+                        className={styles.pfp}
                     />
                 </Link>
-                <Image 
+                <Image
                     src="/navbar-icons/caret-down.png"
                     title="Dropdown"
                     alt=""
