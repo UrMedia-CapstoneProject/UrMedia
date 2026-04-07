6150f8bbced74c5c185b6a1d2d537b15060d3f06
DhruvZaver <ThatIndian15@gmail.com>
2026-04-06 20:41:17 -0500

    Updated MediaCard

--- ur-media-app/src/components/Profile/TrackedMedia/MediaCard.tsx
+++ ur-media-app/src/components/Profile/TrackedMedia/MediaCard.tsx
@@ -3,11 +3,18 @@ import styles from "./MediaCard.module.css"
 import { useState } from "react"
 import Poster from "@/components/Global/Poster";
 
-export default function () {
+type MediaCardProps = {
+  title: string
+  imageUrl: string
+  epWatched: string
+  score?: string
+}
+
+export default function MediaCard({title, imageUrl, epWatched, score} : MediaCardProps) {
     return (
         <div className={styles.main}>
             <div className={styles.poster}>
-                <Poster title={"The Odyssey"} imageUrl={"/test-images/the-odyssey.jpg"} />
+                <Poster title={title} imageUrl={imageUrl} />
                 <div className={styles.mediaInfo}>
                     <div>
                         <p className={styles.title}>
@@ -19,8 +26,8 @@ export default function () {
                     </div>
 
                     <div className={styles.userStats}>
-                        <p className={styles.epWatched}>0</p>
-                        <p className={styles.score}>7/10</p>
+                        <p className={styles.epWatched}>{epWatched}</p>
+                        <p className={styles.score}>{score}</p>
                     </div>
 
                 </div>
@@ -28,3 +35,34 @@ export default function () {
         </div>
     )
 }
+
+// "use client"
+// import styles from "./MediaCard.module.css"
+// import { useState } from "react"
+// import Poster from "@/components/Global/Poster";
+
+// export default function () {
+//     return (
+//         <div className={styles.main}>
+//             <div className={styles.poster}>
+//                 <Poster title={"The Odyssey"} imageUrl={"/test-images/the-odyssey.jpg"} />
+//                 <div className={styles.mediaInfo}>
+//                     <div>
+//                         <p className={styles.title}>
+//                             <span className={styles.titleText}>
+//                                 {"The Odyssey and what if the name is a little longer? and what if the name is a little longer?"}
+//                             </span>
+//                         </p>
+
+//                     </div>
+
+//                     <div className={styles.userStats}>
+//                         <p className={styles.epWatched}>0</p>
+//                         <p className={styles.score}>7/10</p>
+//                     </div>
+
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
