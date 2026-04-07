8fda49267990978675b591d513799cf6264dcbbe
Bethany Rowland <yorkiegirlzoe2001@gmail.com>
2026-04-07 12:35:40 -0500

    Settings Modal
    Deleted podium modal and started work on the settings modal. Trying to overlay an icon on top of button, not successful yet :((((

--- ur-media-app/src/components/Profile/UserBio/UserBio.tsx
+++ ur-media-app/src/components/Profile/UserBio/UserBio.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { useState } from "react"
 import styles from "./UserBio.module.css"
 import Image from "next/image"
 
 export default function () {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={styles.main}>
             <div className={styles.content}>
@@ -21,14 +26,26 @@ export default function () {
                     This is a biography. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
 
-                <Image
-                    src="/profile-icons/settings icon.png"
-                    title="Settings"
-                    alt="Settings"
-                    width={40}
-                    height={40}
-                    className={styles.SettingsIcon}
-                />
+                <div className={styles.SettingsButton}>
+                    <button onClick={() => setIsOpen(true)}>
+                        Open Modal
+                    </button>
+                    <span className={styles.icon}>
+                        <Image
+                            src="/profile-icons/settings.png"
+                            title="Settings"
+                            alt="Settings"
+                            width={45}
+                            height={45}
+                        />
+                    </span>
+                </div>
+
+                {isOpen && (
+                    <div>
+                        <p>Modal content goes here</p>
+                    </div>
+                )}
 
             </div>
         </div>
