01b205794314ded0116bc7f959802350964342e3
Bethany Rowland <yorkiegirlzoe2001@gmail.com>
2026-04-07 11:43:47 -0500

    Podium Modal
    Started on the modal, it is broken rn tho

---
+++ ur-media-app/src/components/Profile/Podium/PodiumModal.tsx
@@ -0,0 +1,14 @@
+"use client"
+import styles from "./PodiumModal.module.css"
+
+type PodiumModalProps = {
+  media: MediaItem | null
+  isOpen: boolean
+  onClose: () => void
+}
+
+export function() {
+    return (
+        
+    )
+}
