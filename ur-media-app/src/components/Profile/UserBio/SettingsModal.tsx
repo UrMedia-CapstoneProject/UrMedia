efecc8245206041c7ddefbea55f78e4f9b36ec38
Bethany Rowland <yorkiegirlzoe2001@gmail.com>
2026-04-09 17:15:46 -0500

    Settings Modal
    Got most of the formatting done for the settings modal, just needs some finishing touches in the css.

--- ur-media-app/src/components/Profile/UserBio/SettingsModal.tsx
+++ ur-media-app/src/components/Profile/UserBio/SettingsModal.tsx
@@ -1,4 +1,6 @@
+import SignOutButton from "@/components/Media/SignOutButton"
 import styles from "./SettingsModal.module.css"
+import Image from "next/image"
 
 type SettingsModalProps = {
   isSettingsOpen: boolean
@@ -10,15 +12,71 @@ export default function SettingsModal({
     onClose
     }: SettingsModalProps) {
 
+    const handleUsernameChange = (value: string) => {}
+    const handleBioChange = (value: string) => {}
+    const setBirthday = (value: string) => {}
+    const handleSave = () => {}
+    const handleCancel = () => {}
+
     return (
+
         <div className={styles.main} onClick={onClose}>
             <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-                <p className={styles.option}>Change Username</p>
-                <p className={styles.option}>Change User Biography</p>
-                <p className={styles.option}>Change Profile Picture</p>
-                <p className={styles.option}>Change Birthday</p>
-                <p className={styles.option}>Sign Out</p>
-                <p className={styles.option}>Delete Account</p>
+                <div className={styles.settingsItem}>
+                    <Image
+                        src="/profile-icons/default icon.png"
+                        alt="Profile Picture"
+                        width={200}
+                        height={200}
+                    />
+                    <label className={styles.option}>Change Profile Picture:</label>
+
+                    <input type="file" id="filePicker" hidden />
+                    <label htmlFor="filePicker" className={styles.filePicker}>Choose File</label>
+                </div>
+
+                <div className={styles.settingsItem}>
+                    <label className={styles.option}>Change User Biography:</label>
+
+                    <textarea
+                        rows={10}
+                        onChange={(e) => handleBioChange(e.target.value)}
+                        placeholder="Tell us about yourself..."
+                        className={styles.textarea}
+                    />
+                </div>
+
+                <div className={styles.settingsItem}>
+                    <label className={styles.option}>Change Username:</label>
+
+                    <input
+                        type="string"
+                        onChange={(e) => handleUsernameChange(e.target.value)}
+                        placeholder="New Username"
+                        className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.settingsItem}>
+                    <label className={styles.option}>Change Birthday:</label>
+
+                    <input
+                        type="date"
+                        onChange={(e) => setBirthday(e.target.value)}
+                        placeholder="mm/dd/yyyy"
+                        className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.exitOptions}>
+                    <SignOutButton />
+                    <label className={styles.deleteAccount}>Delete Account</label>
+                </div>
+
+                <div className={styles.saveAndCancel}>
+                    <button className={styles.cancelButton} onClick={() => handleCancel()}>Cancel</button>
+                    <button className={styles.saveButton} onClick={() => handleSave()}>Save</button>
+                </div>
             </div>
         </div>
     )
