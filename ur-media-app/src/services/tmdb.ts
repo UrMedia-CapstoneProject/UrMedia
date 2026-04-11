a58248845127d83d50488aec531c76b1ed0d9fe3
DhruvZaver <ThatIndian15@gmail.com>
2026-04-11 12:27:57 -0500

    Added Skyler commits for Catalog page

--- ur-media-app/src/services/tmdb.ts
+++ ur-media-app/src/services/tmdb.ts
@@ -1,20 +1,66 @@
-import {TMDBError} from "@lorenzopant/tmdb"
-import {tmdb} from "../lib/tmdb";
+import { TMDBError } from "@lorenzopant/tmdb";
+import { tmdb } from "../lib/tmdb";
 
-export async function getMovies() {
-    try{
-        const res = tmdb.movie_lists.now_playing
-        return res
-    } catch {
-
+export async function getMovie(id: number) {
+  try {
+    const res = await tmdb.movies.details({movie_id: id})
+    return res
+  } catch (err) {
+    if (err instanceof TMDBError) {
+      console.log(err.message);
+      console.log(err.http_status_code);
+      console.log(err.tmdb_status_code);
     }
+  }
+}
+export async function getMovies() {
+  try {
+    const res = await tmdb.movie_lists.now_playing();
+    return res;
+  } catch (err) {
+    if (err instanceof TMDBError) {
+      console.log(err.message);
+      console.log(err.http_status_code);
+      console.log(err.tmdb_status_code);
+    }
+  }
+}
+
+export async function getPopularMovies(page: number) {
+  try {
+    const res = await tmdb.movie_lists.popular({ page });
+    return res
+  } catch (err) {
+    if (err instanceof TMDBError) {
+      console.log(err.message);
+      console.log(err.http_status_code);
+      console.log(err.tmdb_status_code);
+    }
+  }
 }
 
 export async function getShows() {
-    try {
-        const res = tmdb.tv_lists.on_the_air
-        return res
-    } catch {
-
+  try {
+    const res = await tmdb.tv_lists.on_the_air();
+    return res;
+  } catch (err) {
+    if (err instanceof TMDBError) {
+      console.log(err.message);
+      console.log(err.http_status_code);
+      console.log(err.tmdb_status_code);
     }
+  }
+}
+
+export async function getPopularShows(page: number) {
+  try {
+    const res = await tmdb.tv_lists.popular({ page });
+    return res;
+  } catch (err) {
+    if (err instanceof TMDBError) {
+      console.log(err.message);
+      console.log(err.http_status_code);
+      console.log(err.tmdb_status_code);
+    }
+  }
 }
