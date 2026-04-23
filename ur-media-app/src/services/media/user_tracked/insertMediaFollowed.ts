ef33a2b90f67a9a032f3ece402f54f473c1d5d59
DhruvZaver <ThatIndian15@gmail.com>
2026-04-22 21:34:34 -0500

    Update insertMediaFollowed.ts

--- ur-media-app/src/services/media/user_tracked/insertMediaFollowed.ts
+++ ur-media-app/src/services/media/user_tracked/insertMediaFollowed.ts
@@ -1,25 +1,30 @@
 import { TrackMediaPayload } from "./upsertUserTrackedMedia";
 
 export async function insertMediaFollowed({
-    supabase,
-    userId,
-    payload
+  supabase,
+  userId,
+  payload,
 }: {
-    supabase: any,
-    userId: string,
-    payload: TrackMediaPayload
+  supabase: any;
+  userId: string;
+  payload: TrackMediaPayload;
 }) {
-    const media_id = payload.mediaId
+  const { error: mediaFollowsError } = await supabase
+    .from("media_follows")
+    .upsert(
+      {
+        user_id: userId,
+        media_id: payload.mediaId,
+      },
+      {
+        // Ignore the error with the user already following the media.
+        onConflict: "user_id,media_id",
+        ignoreDuplicates: true,
+      },
+    );
 
-    const { error: mediaFollowsError } = await supabase
-        .from("media_follows")
-        .insert({
-            user_id: userId,
-            media_id: media_id
-        })
-
-        if (mediaFollowsError) {
-            console.log("insertMediaFollowed.ts file:", mediaFollowsError.message)
-            throw new Error(mediaFollowsError.message)
-        }
+  if (mediaFollowsError) {
+    console.log("insertMediaFollowed.ts file:", mediaFollowsError.message);
+    throw new Error(mediaFollowsError.message);
+  }
 }
